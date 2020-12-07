e33b37ffdfea5d50372c00d054cf668f4468ea43
sniren210 <sniren2002@gmail.com>
2020-12-07 19:55:16 +0700

    commit pertama

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1607323982,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ground.png",
+		"images/box1-sheet0.png",
+		"images/pig-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
